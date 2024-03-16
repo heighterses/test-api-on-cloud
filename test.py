94935efd9e54e56451f32f037d6f834c7fbeb975
M. Abdullah Bin Aftab <heighter79@gmail.com>
2024-03-16 07:12:38 +0500

    add final code to repo

--- test.py
+++ test.py
@@ -2,8 +2,7 @@ import requests
 
 url_val = input("input after slash: ")
 api_url = f"https://api-tetsing.azure-api.net/{url_val}"
-# key = "FIND_IT_ON_GITHUB_REPO"
-key = "7199a1296d944cd5b294117f513b66a4"
+key = "FIND_IT_ON_GITHUB_REPO"
 headers = {
     "test-key-name": key
 }
