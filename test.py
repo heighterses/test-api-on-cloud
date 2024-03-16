29c2e1c5fefec8381eaa08be1143197c413dcd4e
M. Abdullah Bin Aftab <110668234+heighterses@users.noreply.github.com>
2024-03-16 08:21:11 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import requests
 
+# API KEY --> 7199a1296d944cd5b294117f513b66a4
+
 url_val = input("input after slash: ")
 api_url = f"https://api-tetsing.azure-api.net/{url_val}"
-key = "FIND_IT_ON_GITHUB_REPO"
+key = "FIND_IT_ON_GITHUB_REPO" 
 headers = {
     "test-key-name": key
 }
